97ddc8c43f9ab2c7c84490d8dd7f922f201c46f1
Khadeer-Pathan <pathankhadeer@msitprogram.net>
2022-10-02 02:17:42 +0530

    get_valid_words() is created

--- hangman.py
+++ hangman.py
@@ -1,3 +1,15 @@
 # Hangman Game Project by Khadeer Pathan, IIIT-H
 
 from words import words
+import random
+
+def get_valid_words(words):
+    word = random.choice(words)     # picks a random word from words list
+    for i in word:
+        if i.isalpha() == False:    # every value in the picked up word should be an aplhabet or else pick a new word
+            word = random.choice(words)
+    return word
+
+
+word = get_valid_words(words)
+# print(word)
